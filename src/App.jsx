305ca8e83733dcae80edd495d60b54f3a7fe48e3
sivani-r-anil <sivaniranil28@gmail.com>
2026-07-11 11:43:10 +0530

    Search is added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddRegiststration from './components/AddRegiststration'
+import SearchRegistration from './components/SearchRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <AddRegiststration/>
+      <SearchRegistration/>
     </>
   )
 }
