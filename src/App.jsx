2dea8d7fb39968f165b3326874ea180045866e85
sivani-r-anil <sivaniranil28@gmail.com>
2026-07-11 11:48:16 +0530

    Delete is added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import AddRegiststration from './components/AddRegiststration'
 import SearchRegistration from './components/SearchRegistration'
+import DeleteRegistration from './components/DeleteRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <>
       <AddRegiststration/>
       <SearchRegistration/>
+      <DeleteRegistration/>
     </>
   )
 }
